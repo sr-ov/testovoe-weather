f225f38b8e3e146f1896cb821a2e39813cdf367c
sr-ov <c8@yandex.ru>
2021-11-14 15:08:00 +0400

    done

--- layouts/error.vue
+++ layouts/error.vue
@@ -7,9 +7,12 @@ export default {
 <template>
     <div class="is-size-3 has-text-centered mt-6">
         <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
-        <pre v-else>
-            {{ error }}
-        </pre>
+        <template v-else>
+            <h1>ОШИБКА</h1>
+            <pre>
+                {{ error }}
+            </pre>
+        </template>
         <NuxtLink to="/">На главную</NuxtLink>
     </div>
 </template>
